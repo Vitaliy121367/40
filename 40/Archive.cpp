8f80016414f57f6b3f53585ae7f821e884c0178b
Vitaliy <qw929496@gmail.com>
2025-01-16 17:59:18 +0200

    compareEdition

--- 40/Archive.cpp
+++ 40/Archive.cpp
@@ -2,14 +2,18 @@
 
 Archive::Archive()
 {
-	Catalog.insert(new Book("Harry Potter", "Rowling", true, 2001, "Ranok"));
-	Catalog.insert(new Magazine("Nature", "Ukraine", true, 2024, 2));
-	Catalog.insert(new Magazine("Nature", "Ukraine", true, 2024, 5));
-	Catalog.insert(new Textbook("C++", "Straustrup", true, 2005, "Ranok",8));
+	Catalog.insert(shared_ptr<Edition>( new Book("Harry Potter", "Rowling", true, 2001, "Ranok")));
+	Catalog.insert(shared_ptr<Edition>(new Magazine("Nature", "Ukraine", true, 2024, 2)));
+	Catalog.insert(shared_ptr<Edition>(new Magazine("Nature", "Ukraine", true, 2024, 5)));
+	Catalog.insert(shared_ptr<Edition>(new Textbook("C++", "Straustrup", true, 2005, "Ranok",8)));
 }
 
 Archive::~Archive()
 {
+	/*for (auto item : Catalog)
+	{
+		delete item;
+	}*/
 }
 
 void Archive::showCatalog() const
--- 40/Archive.h
+++ 40/Archive.h
@@ -7,9 +7,35 @@
 #include <algorithm>
 using namespace std;
 
+struct compareEdition {
+	bool operator()(const shared_ptr<Edition> left, const shared_ptr<Edition> right)const {
+		if (left->getYear()> right->getYear())
+		{
+			return true;
+		}
+		else if (left->getYear() == right->getYear())
+		{
+			if (string(typeid(*left).name()) < string(typeid(*right).name()))
+			{
+				return true;
+			}
+			else if(string(typeid(*left).name()) == string(typeid(*right).name()) && string(typeid(*left).name()) == "class Magazine")
+			{
+				return (dynamic_pointer_cast<Magazine>(left))->getNumber() < (dynamic_pointer_cast<Magazine>(right))->getNumber();
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+};
+
 class Archive
 {
-	set<Edition*>Catalog;
+	set<shared_ptr<Edition>, compareEdition>Catalog;
 public:
 	Archive();
 	~Archive();
